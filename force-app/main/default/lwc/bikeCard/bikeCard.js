09dedc0a30d91c701c1e99f2ecd92605cd36c2a9
NagabhushanaCN <nagabhushancn97@gmail.com>
2026-03-31 22:04:48 +0530

    first commit

---
+++ force-app/main/default/lwc/bikeCard/bikeCard.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class BikeCard extends LightningElement {
+    name = 'hayabuza X4';
+    description = 'A sweet bike built for comfort.';
+    category = 'Mountain';
+    material = 'Steel';
+    price = '$2,700';
+    pictureUrl = 'https://th.bing.com/th?id=OIP.AAhgAZbFUO87bULi3dI2egHaFj&w=288&h=216&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2';
+}
